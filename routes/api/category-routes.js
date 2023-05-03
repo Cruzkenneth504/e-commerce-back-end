de1202a04a6233464be5a5fec5bab0e5710225da
kenneth o cruz <cruzkenneth504@gmail.com>
2023-05-03 02:14:39 -0700

    reminder to continue update and create

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -2,19 +2,43 @@ const router = require('express').Router();
 const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
-
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+//get all
+router.get('/', async (req, res) => {
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }]
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+//get one
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!categoryData) {
+      res.status(400).json({ message: 'No Product found with that id!' });
+      return;
+    }
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-router.post('/', (req, res) => {
-  // create a new category
+//create
+router.post('/', async (req, res) => {
+  try {
+    const newCategory = await Category.create({
+      product_id: req.body.product_id,
+    });
+    res.status(200).json(newCategory);
+  }catch(err){
+   res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
